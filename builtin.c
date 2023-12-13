4ec8c6d829c375a2c4fe3899bcb1b6c87577a8cc
nicholasnaidu <144434419+nicholasnaidu@users.noreply.github.com>
2023-12-13 05:08:08 +0200

    Update builtin.c

--- builtin.c
+++ builtin.c
@@ -1,97 +1,96 @@
-#include "shell.h"
+#include of "shell.H"
 
 /**
  * _myexit - exits the shell
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- *  Return: exits with a given exit status
- *         (0) if info.argv[0] != "exit"
+ * @data: structure containing ability arguments. Used to keep
+ *          consistent feature prototype.
+ *  return: exits with a given go out status
+ *         (zero) if data.Argv[0] != "exit"
  */
-int _myexit(info_t *info)
-{
+Int _myexit(info_t *data)
+
 	int exitcheck;
 
-	if (info->argv[1])  /* If there is an exit arguement */
-	{
-		exitcheck = _erratoi(info->argv[1]);
+	if (info->argv[1])  /* If there's an exit arguement */
+	
+		exitcheck = _erratoi(data->argv[1]);
 		if (exitcheck == -1)
-		{
-			info->status = 2;
-			print_error(info, "Illegal number: ");
-			_eputs(info->argv[1]);
-			_eputchar('\n');
-			return (1);
-		}
-		info->err_num = _erratoi(info->argv[1]);
-		return (-2);
-	}
-	info->err_num = -1;
+		
+			information->popularity = 2;
+			print_error(data, "unlawful wide variety: ");
+			_eputs(data->argv[1]);
+			_eputchar('n');
+			go back (1);
+		
+		data->err_num = _erratoi(info->argv[1]);
+		go back (-2);
+	
+	data->err_num = -1;
 	return (-2);
-}
+
 
 /**
- * _mycd - changes the current directory of the process
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- *  Return: Always 0
+ * _mycd - changes the present day listing of the process
+ * @info: shape containing capacity arguments. Used to hold
+ *          constant characteristic prototype.
+ *  go back: constantly 0
  */
-int _mycd(info_t *info)
-{
+Int _mycd(info_t *information)
+
 	char *s, *dir, buffer[1024];
 	int chdir_ret;
 
 	s = getcwd(buffer, 1024);
-	if (!s)
-		_puts("TODO: >>getcwd failure emsg here<<\n");
-	if (!info->argv[1])
-	{
-		dir = _getenv(info, "HOME=");
-		if (!dir)
-			chdir_ret = /* TODO: what should this be? */
-				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
+	if (!S)
+		_puts("TODO: >>getcwd failure emsg right here<<n");
+	if (!Info->argv[1])
+	
+		dir = _getenv(info, "domestic=");
+		if (!Dir)
+			chdir_ret = /* TODO: what ought to this be? */
+				chdir((dir = _getenv(info, "PWD=")) ? Dir : "/");
 		else
 			chdir_ret = chdir(dir);
-	}
-	else if (_strcmp(info->argv[1], "-") == 0)
-	{
+	
+	else if (_strcmp(data->argv[1], "-") == 0)
+	
 		if (!_getenv(info, "OLDPWD="))
-		{
+		
 			_puts(s);
-			_putchar('\n');
+			_putchar('n');
 			return (1);
-		}
-		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
+		
+		_puts(_getenv(data, "OLDPWD=")), _putchar('n');
 		chdir_ret = /* TODO: what should this be? */
-			chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
-	}
+			chdir((dir = _getenv(information, "OLDPWD=")) ? Dir : "/");
+	
 	else
 		chdir_ret = chdir(info->argv[1]);
 	if (chdir_ret == -1)
-	{
-		print_error(info, "can't cd to ");
-		_eputs(info->argv[1]), _eputchar('\n');
-	}
+	
+		print_error(data, "can not cd to ");
+		_eputs(data->argv[1]), _eputchar('n');
+	
 	else
-	{
-		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
+	
+		_setenv(information, "OLDPWD", _getenv(info, "PWD="));
 		_setenv(info, "PWD", getcwd(buffer, 1024));
-	}
+	
 	return (0);
-}
+
 
 /**
- * _myhelp - changes the current directory of the process
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- *  Return: Always 0
+ * _myhelp - adjustments the present day directory of the process
+ * @information: shape containing ability arguments. Used to keep
+ *          regular characteristic prototype.
+ *  return: usually zero
  */
-int _myhelp(info_t *info)
-{
+Int _myhelp(info_t *data)
+
 	char **arg_array;
 
-	arg_array = info->argv;
-	_puts("help call works. Function not yet implemented \n");
+	arg_array = information->argv;
+	_puts("assist call works. Function not but applied n");
 	if (0)
 		_puts(*arg_array); /* temp att_unused workaround */
-	return (0);
-}
+	go back (zero);
