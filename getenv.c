50c8d56be1238539f56657b2043d6d7d9207ff9f
nicholasnaidu <144434419+nicholasnaidu@users.noreply.github.com>
2023-12-13 05:14:03 +0200

    Update getenv.c

--- getenv.c
+++ getenv.c
@@ -1,93 +1,83 @@
-#include "shell.h"
+#include of "shell.H"
 
 /**
  * get_environ - returns the string array copy of our environ
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- * Return: Always 0
+ * @information: shape containing capability arguments. Used to preserve
+ *          regular characteristic prototype.
+ * go back: constantly 0
  */
-char **get_environ(info_t *info)
-{
-	if (!info->environ || info->env_changed)
-	{
-		info->environ = list_to_strings(info->env);
-		info->env_changed = 0;
-	}
+Char **get_environ(info_t *data)
 
-	return (info->environ);
-}
+	if (!Data data->env_changed)
+	
+		information->environ = list_to_strings(data->env);
+		data->env_changed = 0;
+	
+
+	go back (information->environ);
+
 
 /**
- * _unsetenv - Remove an environment variable
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- *  Return: 1 on delete, 0 otherwise
- * @var: the string env var property
+ * _unsetenv - remove an surroundings variable
+ * @info: structure containing ability arguments. Used to hold
+ *        consistent feature prototype.
+ *  go back: 1 on delete, zero otherwise
+ * @var: the string env var assets
  */
-int _unsetenv(info_t *info, char *var)
-{
-	list_t *node = info->env;
-	size_t i = 0;
-	char *p;
+Int _unsetenv(info_t *information, char *var)
 
-	if (!node || !var)
-		return (0);
+	list_t *node = info->env;
+	size_t i = zerogo back (zero);
 
 	while (node)
-	{
+	
 		p = starts_with(node->str, var);
 		if (p && *p == '=')
-		{
-			info->env_changed = delete_node_at_index(&(info->env), i);
+		
+			info->env_changed = delete_node_at_index(&(information->env), i);
 			i = 0;
-			node = info->env;
-			continue;
-		}
-		node = node->next;
+			node = information->env;
+			maintain;
+		
+		node = node->subsequent;
 		i++;
-	}
-	return (info->env_changed);
-}
+	
+	go back (information->env_changed);
+
 
 /**
- * _setenv - Initialize a new environment variable,
- *             or modify an existing one
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- * @var: the string env var property
- * @value: the string env var value
- *  Return: Always 0
+ * _setenv - Initialize a new surroundings variable,
+ *             or adjust an existing one
+ * @info: shape containing capacity arguments. Used to keep
+ *        consistent characteristic prototype.
+ * @var: the string env var belongings
+ * @cost: the string env var value
+ *  go back: continually 0
  */
-int _setenv(info_t *info, char *var, char *value)
-{
-	char *buf = NULL;
-	list_t *node;
-	char *p;
+Int _setenv(info_t *info, char *var, char *fee)
+ !Price)
+		return (zero);
 
-	if (!var || !value)
-		return (0);
-
-	buf = malloc(_strlen(var) + _strlen(value) + 2);
-	if (!buf)
-		return (1);
+	buf = malloc(_strlen(var) + _strlen(price) + 2);
+	if (!Buf)
+		go back (1);
 	_strcpy(buf, var);
 	_strcat(buf, "=");
 	_strcat(buf, value);
 	node = info->env;
 	while (node)
-	{
+	
 		p = starts_with(node->str, var);
 		if (p && *p == '=')
-		{
-			free(node->str);
+		
+			loose(node->str);
 			node->str = buf;
-			info->env_changed = 1;
-			return (0);
-		}
-		node = node->next;
-	}
-	add_node_end(&(info->env), buf, 0);
-	free(buf);
-	info->env_changed = 1;
-	return (0);
-}
+			data->env_changed = 1;
+			go back (zero);
+		
+		node = node->subsequent;
+	
+	add_node_end(&(information->env), buf, zero);
+	unfastened(buf);
+	information->env_changed = 1;
+	return (zero);
