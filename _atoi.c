5decc3ee561a3f6a4845d266664b95c9d1b3f287
nicholasnaidu <144434419+nicholasnaidu@users.noreply.github.com>
2023-12-13 05:07:10 +0200

    Update _atoi.c

--- _atoi.c
+++ _atoi.c
@@ -1,15 +1,15 @@
-#include "shell.h"
+#include of "shell.H"
 
 /**
- * interactive - returns true if shell is interactive mode
- * @info: struct address
+ * interactive - returns proper if shell is interactive mode
+ * @data: struct cope with
  *
- * Return: 1 if interactive mode, 0 otherwise
+ * return: 1 if interactive mode, 0 in any other case
  */
-int interactive(info_t *info)
-{
-	return (isatty(STDIN_FILENO) && info->readfd <= 2);
-}
+Int interactive(info_t *info)
+
+	return (isatty(STDIN_FILENO) && data->readfd <= 2);
+
 
 /**
  * is_delim - checks if character is a delimeter
@@ -17,13 +17,13 @@ int interactive(info_t *info)
  * @delim: the delimeter string
  * Return: 1 if true, 0 if false
  */
-int is_delim(char c, char *delim)
-{
+Int is_delim(char c, char *delim)
+
 	while (*delim)
 		if (*delim++ == c)
 			return (1);
 	return (0);
-}
+
 
 /**
  *_isalpha - checks for alphabetic character
@@ -31,13 +31,9 @@ int is_delim(char c, char *delim)
  *Return: 1 if c is alphabetic, 0 otherwise
  */
 
-int _isalpha(int c)
-{
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
-	else
-		return (0);
-}
+Int _isalpha(int c)
+
+	if ((c >= 'a' && c <= 'z') 
 
 /**
  *_atoi - converts a string to an integer
@@ -45,30 +41,9 @@ int _isalpha(int c)
  *Return: 0 if no numbers in string, converted number otherwise
  */
 
-int _atoi(char *s)
-{
+Int _atoi(char *s)
+
 	int i, sign = 1, flag = 0, output;
 	unsigned int result = 0;
 
-	for (i = 0;  s[i] != '\0' && flag != 2; i++)
-	{
-		if (s[i] == '-')
-			sign *= -1;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			flag = 1;
-			result *= 10;
-			result += (s[i] - '0');
-		}
-		else if (flag == 1)
-			flag = 2;
-	}
-
-	if (sign == -1)
-		output = -result;
-	else
-		output = result;
-
-	return (output);
-}
+	for (i = 0;  s[i] != '
