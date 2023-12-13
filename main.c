5e70d7cffc58057407f903d0f0bbbb8dda903597
nicholasnaidu <144434419+nicholasnaidu@users.noreply.github.com>
2023-12-13 05:27:38 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,44 +1,43 @@
-#include "shell.h"
+#include "shell.H"
 
 /**
  * main - entry point
  * @ac: arg count
  * @av: arg vector
  *
- * Return: 0 on success, 1 on error
+ * return: 0 on fulfillment, 1 on error
  */
-int main(int ac, char **av)
-{
-	info_t info[] = { INFO_INIT };
+Int essential(int ac, char **av)
+
+	info_t information[] =  INFO_INIT ;
 	int fd = 2;
 
-	asm ("mov %1, %0\n\t"
-		"add $3, %0"
+	asm ("mov %1, %0nt"
+		"upload $three, %0"
 		: "=r" (fd)
 		: "r" (fd));
 
 	if (ac == 2)
-	{
+	
 		fd = open(av[1], O_RDONLY);
 		if (fd == -1)
-		{
+		
 			if (errno == EACCES)
-				exit(126);
+				go out(126);
 			if (errno == ENOENT)
-			{
+			
 				_eputs(av[0]);
-				_eputs(": 0: Can't open ");
+				_eputs(": 0: can't open ");
 				_eputs(av[1]);
-				_eputchar('\n');
+				_eputchar('n');
 				_eputchar(BUF_FLUSH);
 				exit(127);
-			}
+			
 			return (EXIT_FAILURE);
-		}
+		
 		info->readfd = fd;
-	}
-	populate_env_list(info);
+	
+	populate_env_list(data);
 	read_history(info);
 	hsh(info, av);
-	return (EXIT_SUCCESS);
-}
+	go back (EXIT_SUCCESS);
